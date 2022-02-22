7034aad9ac4a34f5b9fe6404a9a30abcad47ad18
Eyobedk <EyobedKebede10@gmail.com>
2022-02-22 17:17:23 +0000

    entry point

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require('express');
-app = express();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const content ={
+app = express();
+app.use(express.json());
+
+
+const content = {
     "name": "ethiopiawi app",
-    "title":"about ethiopia"
+    "title": "about ethiopia"
 }
 
+app.get('/posts', (req, res) => {
+    res.json(content);
+});
+
+app.post('/login', (req, res) => {
+    //Auth
+    var username = req.body.username;
+
+    res.json({
+        accessToken: Tokenize(username)
+    })
+});
+
+
+function Tokenize(user){
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET_KEY);
+    return accessToken;
+}
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+});
