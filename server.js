4791004c2ca83649694abe31470faf8f642f1b15
Eyobedk <EyobedKebede10@gmail.com>
2022-02-22 19:00:45 +0000

    to be fixed

--- server.js
+++ server.js
@@ -6,27 +6,40 @@ app = express();
 app.use(express.json());
 
 
-const content = {
-    "name": "ethiopiawi app",
-    "title": "about ethiopia"
-}
+const content = [{
+        name: "ethiopiawi app",
+        title: "about ethiopia",
+    },
+    {
+        name: "auropawi app",
+        title: "about europe"
+    }
 
-app.get('/posts', Tokenize, (req, res) => {
-    res.json(content);
+]
+
+app.get('/posts', AuthenticateToken, (req, res) => {
+    res.json(content.filter(cont => cont.name === req.body.name));
 });
 
 app.post('/login', (req, res) => {
     //Auth
     var username = req.body.username;
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET_KEY);
+    const accessToken = jwt.sign(username, process.env.ACCESS_TOKEN_SECRET_KEY);
     res.json({
         accessToken: accessToken
     })
 });
 
 
-function Tokenize(req, res, next){
-    const vari = jwt.verify()
+function AuthenticateToken(req, res, next) {
+    const authHeaders = req.headers['authorization'];
+    const token = authHeaders && authHeaders.split(' ')[1];
+    if (token == null) return res.status(401);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    })
 }
 
 app.listen(3000, () => {
