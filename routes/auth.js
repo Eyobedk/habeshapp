67c0b5e70c754a084e9c0518c0d90345aed4313f
Eyobedk <EyobedKebede10@gmail.com>
2022-02-23 18:34:50 +0000

    refresh token

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,22 @@
 const joi = require('@hapi/joi');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const {validateSchema} = require('../models/validation');
-
+const {
+    validateSchema
+} = require('../models/validation');
+const router = require('express').Router();
+require('dotenv').config();
 var data = {};
+var refereshTokens = [];
+exports.register = async (req, res) => {
 
-exports.register = async (req,res)=>{
-    
-    const {error} = validateSchema(req.body);
-    if(error) return res.status(400).send(error.details[0].message)
+    const {
+        error
+    } = validateSchema(req.body);
+    if (error) return res.status(400).send(error.details[0].message)
 
     //PERFORM CHECK EMAIL_EXISTS,
-    if((Object.keys(data).find(key => data[key] === req.body.email))) return res.status(400).send("email exists")
+    if ((Object.keys(data).find(key => data[key] === req.body.email))) return res.status(400).send("email exists")
 
     //ENCRYPT PASSWORD_WITH-BCRYPT,
     const salt = await bcrypt.genSalt(10);
@@ -24,18 +29,39 @@ exports.register = async (req,res)=>{
     res.json(data);
 }
 
-exports.login = async (req,res)=>{
-    const {error} = validateSchema(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-    
+exports.login = async (req, res) => {
+    const {
+        error
+    } = validateSchema(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
     //PERFORM CHECK EMAIL_EXISTS,
-    if(!(Object.keys(data).find(key => data[key] === req.body.email))) return res.status(400).send("email does not exists")
+    if (!(Object.keys(data).find(key => data[key] === req.body.email))) return res.status(400).send("email does not exists")
 
     //DECRYPT PASSWORD_WITH-BCRYPT AND CHECK PASSWORD,
     const compared = await bcrypt.compare(req.body.password, data.password)
-    if(!compared) return res.send("invalid password");
+    if (!compared) return res.send("invalid password");
     //ELSE LOGIN  BUT NOW WE USE JWT
-    const token = jwt.sign({name:data.name}, process.env.ACCESS_TOKEN_SECRET_KEY);
-    res.header('access-token-header',token).send(token)
+    const token = jwt.sign({
+        name: data.name
+    }, process.env.ACCESS_TOKEN_SECRET_KEY, {
+        expiresIn: '30s'
+    });
+    const refreshToken = jwt.sign({
+        name: data.name
+    }, process.env.REFRESH_TOKEN_SECRET_KEY);
+    refereshTokens.push(refreshToken);
+    res.header('access-token-header', token).send(token);
     //res.send('Login');
 }
+
+
+router.post('/token', (req, res) => {
+    const refereshToken = rew.body.token;
+    if(refereshToken == null) return res.status(401).send();
+    if(!(refereshTokens.includes(refereshToken)) ) return res.status(403).send();
+    jwt.verify(refereshToken, process.env.REFRESH_TOKEN_SECRET_KEY,(err,user)=>{
+        if(err) return res.status(403).send();
+        
+    });
+})
