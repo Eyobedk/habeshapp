d53986f23fbc1d8ca03fe2d65bba38ae3e337b51
Eyobedk <EyobedKebede10@gmail.com>
2022-02-27 17:48:32 +0000

    using sessions

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const {register, login,tokenize,deleter} = require('./routes/auth');
+const cors = require('cors');
+const {register,login,tokenize,deleter} = require('./routes/auth');
 const publishRoute = require('./routes/publish');
 require('dotenv').config();
 
 app = express();
 app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}))
 
 
 app.post('/register', register);
 app.post('/login', login);
-app.get('/',publishRoute);
-app.post('/token',tokenize)
-app.delete('/logout',deleter)
-app.get('/forgot-password')
+app.get('/', publishRoute);
+app.post('/token', tokenize)
+app.delete('/logout', deleter)
+app.get('/forgot-password', )
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
