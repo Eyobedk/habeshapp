d741c5e698ff91cd60f5c5feeddf89d723fcae3e
Eyobedk <EyobedKebede10@gmail.com>
2022-02-28 23:26:54 +0000

    signup and login pages

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
 const {Router} = require("express");
-
+const {signup_Get,signup_Post,login_Get,login_Post} = require('../controllers/auth');
 const router = Router();
 
-router.get('/signup',()=>{})
-router.post('/signup',()=>{})
-router.get('/login',()=>{})
-router.post('/login',()=>{})
+router.get('/signup',signup_Get)
+router.post('/signup',signup_Post)
+router.get('/login',login_Get)
+router.post('/login',login_Post)
 module.exports = router;
