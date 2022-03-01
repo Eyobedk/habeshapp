dee8e43c0d9684405b781eb4305ff7d1aa07f836
Eyobedk <EyobedKebede10@gmail.com>
2022-03-01 17:26:06 +0000

    login and signup finished

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,7 +49,7 @@ module.exports.login_Post = async (req, res) => {
 
     try {
       const user = await User.login(email, password);
-      res.status(200).json({ user: user._id });
+      res.json({ user: user._id });
     } catch (err) {
       res.status(400).json({});
     }
