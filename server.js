6443d77f416eec28e5d9ad1468cbff28f8eada04
Eyobedk <EyobedKebede10@gmail.com>
2022-03-10 17:54:02 +0000

    google sign in with token

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const passport = require('passport');
+const {
+    Strategy
+} = require('passport-google-oauth20');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
 const cookieParser = require('cookie-parser');
-const {requireAuth} = require('./middleware/auth');
-const {checkUser} = require('./middleware/checkUser');
+const {
+    requireAuth
+} = require('./middleware/auth');
+const {
+    checkUser
+} = require('./middleware/checkUser');
 
 app = express();
+
+function verifyCallBack(accToken, refoken, profile, done) {
+    console.log('google profile', profile)
+    console.log('access Token', accToken)
+    console.log('ref Token', refoken)
+    done(null, profile)
+}
+passport.use(new Strategy({
+    callbackURL: '/google/callback',
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
+}, verifyCallBack))
+
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 app.use(express.json());
@@ -15,6 +37,7 @@ app.use(express.urlencoded({
     extended: false
 }))
 app.use(cookieParser())
+app.use(passport.initialize()) //passport session
 
 async function main() {
     await mongoose.connect(process.env.dbURL, {
@@ -28,20 +51,32 @@ async function main() {
         .catch((err) => console.log(err));
 
 
-app.use('*', checkUser)
-app.get('/smoothies', requireAuth, (req, res) => {
-    res.render('smoothies', {
-    user: res.locals.user
+    app.use('*', checkUser)
+    app.get('/smoothies', requireAuth, (req, res) => {
+        res.render('smoothies', {
+            user: res.locals.user
         })
     })
-app.get('/gooAc',(req,res)=>{
-    console.log('from google params: '+req.params)
-    console.log('from google body: '+req.body)
-    res.send("recived")
-})
-app.get('/home',requireAuth,(req,res)=>{res.send("<h1>HOME PAGE</h1>")})
-app.get('/',(req,res)=>{
-    res.sendFile('./public/html/google.html',{root:"./"})})
-app.use(authRoutes)
+    app.get("/auth/google", passport.authenticate('google',{scope:['email']}))
+    app.get('/google/callback', passport.authenticate('google', {
+        failureRedirect: '/failure',
+        successRedirect: '/',
+        session: false
+    }), (req, res) => {
+        console.log('google called us back');
+        res.send("here")
+    });
+    app.get('/failure', (req, res) => {
+        res.send("failed to login")
+    })
+    app.get('/home', requireAuth, (req, res) => {
+        res.send("<h1>HOME PAGE</h1>")
+    })
+    app.get('/', (req, res) => {
+        res.sendFile('./public/html/google.html', {
+            root: "./"
+        })
+    })
+    app.use(authRoutes)
 }
 main();
