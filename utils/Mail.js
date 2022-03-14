95a01e39172d8273bfed80fccd5e0cb9941c52cf
Eyobedk <EyobedKebede10@gmail.com>
2022-03-14 17:29:37 +0000

    add mailer server with gmail service

--- utils/Mail.js
+++ utils/Mail.js
@@ -1,32 +1,24 @@
 const nodemailer = require('nodemailer');
-const path = require('path');
-require('dotenv').config()
-
-async function main() {
-    let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, // true for 465, false for other ports
+  
+let transporter = nodemailer.createTransport({
+        service: 'gmail',
         auth: {
-            user: "eyobedkebede10@gmail.com", // generated ethereal user
-            pass: "eyobed@403", // generated ethereal password
-        },
-    });
-
-
-
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: '"Fred Foo 👻" eyobedkebede10@gmail.com', // sender address
-        to: "leinternet0@gmail.com", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
-    });
-
-    console.log("Message sent: %s", info.messageId);
-
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-
+            user: "eyobedkebede10@gmail.com",
+            pass: "eyobed@403"
+        }
+});
+  
+let message = {
+    from: "eyobedkebede10@gmail.com",
+    to: "leinternet0@gmail.com",
+    subject: "Subject",
+    html: "<h1>Hello SMTP Email</h1>"
 }
-main();
+  
+transporter.sendMail(message, function(err, info) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(info);
+  }
+})
