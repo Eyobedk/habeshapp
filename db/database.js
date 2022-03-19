869921ededb780ed7125684f173d6d5c469cd952
Eyobed kebede <Eyobedkebede10@gmail.com>
2022-03-20 00:55:27 +0300

    added connection handling for middleware

--- db/database.js
+++ db/database.js
@@ -1,14 +1,5 @@
-// get the client
 const mysql = require('mysql2');
 require('dotenv').config();
-// create the connection to database
-// const connection = mysql.createConnection({
-//   host: process.env.HOST,
-//   user: process.env.USER,
-//   password: 'eyobed@moodOf404',
-//   port: process.env.PORT,
-//   database: process.env.DATABASE
-// });
 
 const pool = mysql.createPool({
   host: process.env.HOST,
@@ -18,3 +9,28 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0
 });
+
+pool.getConnection((err,conn)=>{
+  if(err)
+  {
+    if(err.code =='PROTOCOL_CONNECTION_LOST'){
+      console.log('database connection was closed');
+    }
+    if(err.code == 'CON_COUNT_ERROR')
+    {
+      console.log('database has too many connections')
+    }
+    if(err.code =='ECONNREFUSED')
+    {
+      console.error('database connection was refused')
+    }
+
+    if(conn)
+    {
+      conn.release()
+      return
+    }
+  }
+})
+
+module.exports = pool
