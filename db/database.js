50894b6f712fcdd04d0085432e91b6297ab26e59
Eyobed kebede <Eyobedkebede10@gmail.com>
2022-03-20 00:29:30 +0300

    useing connection pool

--- db/database.js
+++ db/database.js
@@ -2,20 +2,19 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 // create the connection to database
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: process.env.HOST,
+//   user: process.env.USER,
+//   password: 'eyobed@moodOf404',
+//   port: process.env.PORT,
+//   database: process.env.DATABASE
+// });
+
+const pool = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
-  password: 'eyobed@moodOf404',
-  port: process.env.PORT,
-  database: process.env.DATABASE
+  database: process.env.DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
-
-connection.connect(err => {
-      if(err)
-      {
-          return console.log("error: " + err);
-      }
-
-      console.log('connected to the database');
-  })
-  
