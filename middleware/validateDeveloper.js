0c6ce0f451ff26a70f8778380ff36c647f614481
Eyobedk <EyobedKebede10@gmail.com>
2022-04-22 11:43:00 +0100

    error fixed

--- middleware/validateDeveloper.js
+++ middleware/validateDeveloper.js
@@ -1,7 +1,10 @@
-const {
-    validateEmail
-} = require('./validateApps')
-
+function validateEmail(email) {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
 exports.validDevInput = (req, res,next) => {
     const {name,phone,domain,email,password} = req.body;
 
