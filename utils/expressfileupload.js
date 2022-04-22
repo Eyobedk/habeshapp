21822283044b9ced80b61107c272efb2eb8bea47
Eyobedk <EyobedKebede10@gmail.com>
2022-04-22 08:56:19 +0100

    express file upload

--- utils/expressfileupload.js
+++ utils/expressfileupload.js
@@ -1,12 +1,12 @@
 
 "use strict";
 exports.fileuploader = (req, res)=>{
-    const theapk = req.files.screenshots;
-    console.log("theapk.name"+theapk[0])
-    const uploadPath = `uploads/${theapk}`
+    const theapk = req.files.apk;
+    console.log("theapk.name"+theapk.name);
+    const uploadPath = `uploads/${theapk.name}`
 
     theapk.mv(uploadPath, (err)=>{
         if(err){console.log(err)}
         res.send("sent")
-    });
+    })
 }
