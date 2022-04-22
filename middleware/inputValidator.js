a27f6c4c57ba5dd0afd5c67af78ed58dbecf1750
Eyobedk <EyobedKebede10@gmail.com>
2022-04-22 11:41:03 +0100

    validate filies extened

--- middleware/inputValidator.js
+++ middleware/inputValidator.js
@@ -1,8 +1,15 @@
 const {check, validationResult} = require('express-validator')
-const {validateEmail} = require('./validateApps')
 const User = require('../models/User');
 const bcrypt =require('bcrypt');
 
+
+function validateEmail(email) {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
 exports.validateSignupInput = (req, res, next)=>{
     const {name,email,password1,password2} = req.body;
     var result = password1.localeCompare(password2);
