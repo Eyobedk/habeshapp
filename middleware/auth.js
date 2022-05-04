3e15f267a71ec9dcc737f801d4b1197c01869ad9
Eyobedk <EyobedKebede10@gmail.com>
2022-05-04 04:49:24 +0100

    sign google acc res.locals

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,9 @@ const requireAuth =(req, res, next) => {
     const token = req.cookies.jwt;
     const authenicatedUser = req.isAuthenticated() && req.user;
     if (authenicatedUser) {
+        console.log("res.user")
+       // res.locals.user
+        console.log(req.user)
         next();//Google-authenticated
     }else if(token)
     { jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY,(err,decodedToken)=>{
