2b11666dba870da6f4a054178722cd0ec1052aab
Eyobedk <EyobedKebede10@gmail.com>
2022-05-09 20:43:09 +0100

    counts each users view by their id

--- controllers/developer/contViews.js
+++ controllers/developer/contViews.js
@@ -1,6 +1,19 @@
+const Apps = require('../../models/App')
 
 
+exports.contViews = async (req, res, next)=>{
+    const userId = res.locals.userId;
+    const theUser = await Apps.GetAlreadyVistedId(userId, req.params.appid);
+    if(theUser.length == 0)
+    {
+        await Apps.updateViewCount(req.params.appid).then(async()=>{
+            await Apps.UpdateAlreadyVistedId(userId, req.params.appid).catch((err)=>{
+                console.log(err)
+            })
+        }).catch((err)=>{
+            console.log(err)
+        });
 
-exports.contViews = (req, res)=>{
-    
+    }
+    next();
 }
--- routes/users/user.js
+++ routes/users/user.js
@@ -10,6 +10,7 @@ const {requireAuth} = require('../../middleware/auth');
 const {DownloadApp} = require('../../controllers/users/Downloader')
 const {HandleLoadAllCommnets } = require("../../controllers/users/comment")
 const {ReportApp } = require("../../controllers/users/Reports")
+const {contViews } = require("../../controllers/developer/contViews")
 
 
 
@@ -30,7 +31,7 @@ router.post('/reset-password',setNewPassword)
 
 
 router.get('/info', requireAuth,AppInfo)
-router.get('/info/:appid', requireAuth,AppInfo)
+router.get('/info/:appid', requireAuth,contViews,AppInfo)
 router.post('/info/:appid', requireAuth,ReportApp,AddComment,AddRate)
 router.get('/comments/:id', requireAuth, HandleLoadAllCommnets)
 router.get('/download/:id', requireAuth, DownloadApp)
