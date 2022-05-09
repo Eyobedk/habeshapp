e421d5abf1f0679c48ca8ba6ef00b2479fec0f00
Eyobedk <EyobedKebede10@gmail.com>
2022-05-09 20:45:51 +0100

    added reporting malecious apps

--- controllers/users/Reports.js
+++ controllers/users/Reports.js
@@ -2,15 +2,25 @@ const Apps = require('../../models/App')
 
 
 exports.ReportApp = async (req, res,next)=>{
-    if(req.body.report){
-
-        console.log(req.body.report)
+    const reported = req.body.report;
+    let avarageReport;
+    if(reported){
         const ReportedList = await Apps.checkAppisReported(req.params.appid, res.locals.userId);
         if(ReportedList.length == 0)
         {
-            const RatesandDownloads = await Apps.GetRateandDownload(req.params.appid);
-            console.log("RatesandDownloads[0][0]")
-            console.log(RatesandDownloads)
+            await Apps.ReportApp(req.params.appid)
+            await Apps.InserttoReporteds(res.locals.userId, req.params.appid)
+
+            const [RatesandDownloads, _] = await Apps.GetReportandDownload(req.params.appid);
+            avarageReport = parseFloat(RatesandDownloads.appReports / RatesandDownloads.downloads);
+
+            if(avarageReport >= RatesandDownloads.downloads)
+            {
+                await Apps.AddtoblackListAppsTable(req.params.appid, RatesandDownloads.dev_id).catch((err)=>{
+                    console.log(err);
+                });
+            }
+            
         }else{}
     
 }
