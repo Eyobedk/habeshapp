fb2885d63951ae1b29ab789ef8ee734c1c00a24e
Eyobedk <EyobedKebede10@gmail.com>
2022-05-13 18:13:22 +0100

    removed back prop

--- models/App.js
+++ models/App.js
@@ -2,14 +2,13 @@ const db = require('../db/database')
 
 class Apps {
     constructor(appName, catagory, description, appLocation,
-        icon, screenshots, backgroundImage,devId,functOne,functTwo,functThree,functFour) {
+        icon, screenshots,devId,functOne,functTwo,functThree,functFour) {
         this.appName = appName;
         this.catagory = catagory,
         this.description = description,
         this.appLocation = appLocation;
         this.icon = icon;
         this.screenshots = screenshots;
-        this.backgroundImage = backgroundImage;
         this.dev_id = devId;
         this.funcOne = functOne,
         this.funcTwo = functTwo,
@@ -27,10 +26,10 @@ class Apps {
         const date = `${year}-${month}-${day}`;
 
         try {
-            let sql = `INSERT INTO apps(appName,catagory,description,appLocation,icon,screenshotOne,screenshotTwo,screenshotThree,backgroundImage,publishedDate,dev_id,
+            let sql = `INSERT INTO apps(appName,catagory,description,appLocation,icon,screenshotOne,screenshotTwo,screenshotThree,publishedDate,dev_id,
                 funcOne,funcTwo,funcThree,funcFour) 
              VALUES ('${this.appName}','${this.catagory}','${this.description}','${this.appLocation}','${this.icon}',
-            '${this.screenshots[0]}','${this.screenshots[1]}','${this.screenshots[2]}','${this.backgroundImage}','${date}',${this.dev_id},'${this.funcOne}',
+            '${this.screenshots[0]}','${this.screenshots[1]}','${this.screenshots[2]}','${date}',${this.dev_id},'${this.funcOne}',
             '${this.funcTwo}','${this.funcThree}','${this.funcfour}');`;
             return new Promise((resolve, reject) => {
                 resolve(db.execute(sql));
@@ -75,7 +74,7 @@ class Apps {
             console.log(err)
         }
     }
-    static async UpdateTheAppTable(ApkLocation, description, screenshotsArray,newbackgroundImage,ApkID,funcOne,funcTwo,funcThree,funcFour)
+    static async UpdateTheAppTable(ApkLocation, description, screenshotsArray,ApkID,funcOne,funcTwo,funcThree,funcFour)
     {
         const d = new Date();
         const year = d.getFullYear();
@@ -86,7 +85,7 @@ class Apps {
             
             let sql = `UPDATE apps
             SET description = '${description}', appLocation = '${ApkLocation}', screenshotOne = '${screenshotsArray[0]}', screenshotTwo = '${screenshotsArray[1]}',
-            screenshotThree = '${screenshotsArray[2]}',backgroundImage = '${newbackgroundImage}',publishedDate = '${updatedDate}',funcOne = '${funcOne}',
+            screenshotThree = '${screenshotsArray[2]}',publishedDate = '${updatedDate}',funcOne = '${funcOne}',
             funcTwo = '${funcTwo}',funcThree = '${funcThree}',funcFour = '${funcFour}' WHERE appid = ${ApkID};`
             return new Promise((resolve, reject) => {
                 resolve(db.execute(sql));
