68bdc39e92151524ff879d6bb73f6078d2e34384
Eyobedk <EyobedKebede10@gmail.com>
2022-05-23 15:37:09 +0100

    admin routes added
    admin routes added

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -4,7 +4,11 @@ const {sendEmail} = require('../../controllers/admin/sendEmail')
 const {resetAdminPassword} = require('../../controllers/admin/resetAdminPassword')
 const {LoadUsersAndDevelopers} = require('../../controllers/admin/loadUsersandDevs')
 const {ListofBadApps} = require('../../controllers/admin/listMaleciousApps')
-const {deleteReportedApp} = require('../../controllers/admin/deleteReportedApp')
+const {ListBadDevelopers} = require('../../controllers/admin/ListBadDevelopers')
+const {BlackListDeveloper} = require('../../controllers/admin/BlackListDeveloper')
+const {deleteReportedApp} = require('../../controllers/admin/deleteReportedApp');
+const { AlertUser, DeleteAlert} = require('../../controllers/admin/alertUser');
+const { AlertDeveloper, DeleteAlertofDev} = require('../../controllers/admin/alertDeveloper');
 const router = Router();
 
 
@@ -21,7 +25,16 @@ router.post('/admin200/login', logMeIn);
 
 router.get('/admin200/badApps', ListofBadApps)
 router.get('/admin200/removeApp/:id', deleteReportedApp)
+router.get('/admin200/badDevelopers', ListBadDevelopers)
+router.get('/banDev/:devID', BlackListDeveloper)
 
+router.get('/admin200/alertUser', (req, res)=>{res.render('admin/alertUser')})
+router.post('/admin200/alertUser', AlertUser)
+router.post('/admin200/deleteAlert', DeleteAlert)
+
+router.get('/admin200/alertDev', (req, res)=>{res.render('admin/alertDev')})
+router.post('/admin200/alertDev', AlertDeveloper)
+router.post('/admin200/deleteDevAlert', DeleteAlertofDev)
 
 
 
