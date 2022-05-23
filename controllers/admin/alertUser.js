259ab7c336895f9c2777c04674fc4b039146088a
Eyobedk <EyobedKebede10@gmail.com>
2022-05-23 15:33:35 +0100

    Alert users
    Alert users

---
+++ controllers/admin/alertUser.js
@@ -0,0 +1,39 @@
+const Admin = require("../../models/Admin");
+
+
+
+
+
+exports.AlertUser = async (req, res)=>
+{
+    const {message} = req.body;
+    const checkAlreadyPosted = await Admin.checkThereIsaMessage();
+    console.log(checkAlreadyPosted[0])
+    if( typeof checkAlreadyPosted[0] != undefined)
+    {
+        await Admin.AlertUser(message).then(()=>{
+        res.render('admin/alertUser',{response:"message is published to users to be seen"})
+         });
+    }else
+    {
+        console.log("else")
+        await Admin.AlertUserByUpdate(message).then(()=>{
+            res.render('admin/alertUser',{response:"message is published to users to be seen"})
+        });
+    }
+    
+}
+exports.DeleteAlert = async (req, res)=>
+{
+    const deleteMessage = Object.keys(req.body);
+    console.log(deleteMessage);
+
+    const checkAlreadyPosted = await Admin.checkThereIsaMessage();
+    if(!(checkAlreadyPosted.length == 0))
+    {
+        await Admin.DeleteTheMessage().then(()=>{
+        res.render('admin/alertUser',{response:"message is removed from the users page"})
+         });
+    }
+    
+}
