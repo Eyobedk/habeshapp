322ac258318ee884561abc87d6a3a181be951446
Eyobedk <EyobedKebede10@gmail.com>
2022-05-23 15:36:22 +0100

    added new methods to executes sql
    added new methods to executes sql

--- models/Admin.js
+++ models/Admin.js
@@ -29,6 +29,51 @@ class Admin {
     }
   }
 
+  static async blackListDeveloper(theBadDevId) {
+    const sql = `INSERT INTO baneddevelopers(developer_id) VALUES('${theBadDevId}');`;// 
+    const [result, _] = await db.execute(sql).catch((err)=>{console.log(err)});
+    return result;
+  }
+  static async AlertUser(messgae) {
+    const sql = `INSERT INTO alertuser(alertuserid, message) VALUES(200, '${messgae}');`;// 
+    const [result, _] = await db.execute(sql).catch((err)=>{console.log(err)});
+    return result;
+  }
+  static async AlertDev(messgae) {
+    const sql = `INSERT INTO aletdeveloper(messageId, message) VALUES(200, '${messgae}');`;// 
+    const [result, _] = await db.execute(sql).catch((err)=>{console.log(err)});
+    return result;
+  }
+  static async DeleteTheMessage() {
+    const sql = `DELETE FROM alertuser WHERE alertuserid = 200;`;// 
+    const [result, _] = await db.execute(sql).catch((err)=>{console.log(err)});
+    return result;
+  }
+  static async DeleteTheMessagefDev() {
+    const sql = `DELETE FROM aletdeveloper WHERE messageId = 200;`;// 
+    const [result, _] = await db.execute(sql).catch((err)=>{console.log(err)});
+    return result;
+  }
+  static async AlertUserByUpdate(messgae) {
+    const sql = `UPDATE alertuser SET message = '${messgae}' WHERE alertuserid = 200;`;// 
+    const [result, _] = await db.execute(sql).catch((err)=>{console.log(err)});
+    return result;
+  }
+  static async AlertDevByUpdate(messgae) {
+    const sql = `UPDATE aletdeveloper SET message = '${messgae}' WHERE messageId = 200;`;// 
+    const [result, _] = await db.execute(sql).catch((err)=>{console.log(err)});
+    return result;
+  }
+  static async checkThereIsaMessage() {
+    const sql = `SELECT * FROM alertuser WHERE alertuserid = 200;`;// 
+    const [result, _] = await db.execute(sql).catch((err)=>{console.log(err)});
+    return result;
+  }
+  static async checkThereIsaMessageforDev() {
+    const sql = `SELECT * FROM aletdeveloper WHERE messageId = 200;`;// 
+    const [result, _] = await db.execute(sql).catch((err)=>{console.log(err)});
+    return result;
+  }
   static async ListUsers() {
     const sql = `SELECT * FROM habeshapp.user ORDER BY currentdate ASC;`;// 
     const [result, _] = await db.execute(sql);
@@ -44,11 +89,29 @@ class Admin {
     const [result, _] = await db.execute(sql);
     return result;
   }
+  static async checkDeveloperIsBlackListed(id) {
+    const sql = `SELECT * FROM baneddevelopers WHERE developer_id = ${id};`;// 
+    const [result, _] = await db.execute(sql).catch((err)=>{console.log(err)});
+    return result;
+  }
   static async ListofReportedApps() {
     const sql = `SELECT * FROM blacklistedapps;`;// 
     const [result, _] = await db.execute(sql);
     return result;
   }
+  static async ListofBadDevelopers() {
+    const sql = `SELECT * FROM developer WHERE dev_ban_status >= 2;`;
+    const [result, _] = await db.execute(sql);
+    return result;
+  }
+  static async updateBanStatus(id)
+  {
+    const sql = `UPDATE habeshapp.developer SET dev_ban_status =${0} WHERE dev_id = ${id};`;
+    await db.execute(sql).then((result)=>{
+      console.log("admin result"+JSON.stringify(result))
+    }).catch((err)=>{console.log(err)});
+  }
+  
 
   static async login(email, password) {
 
@@ -70,6 +133,7 @@ class Admin {
       console.log("admin result"+JSON.stringify(result))
     }).catch((err)=>{console.log(err)});
   }
+
 }
 
 
