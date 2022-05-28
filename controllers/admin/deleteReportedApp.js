39657391c5a06aa79809da5a3eb5298f9c8d51a3
Eyobedk <EyobedKebede10@gmail.com>
2022-05-28 06:57:19 +0100

    delete updated apps also
    delete updated apps also

--- controllers/admin/deleteReportedApp.js
+++ controllers/admin/deleteReportedApp.js
@@ -8,18 +8,19 @@ exports.deleteReportedApp = async (req, res) => {
         {
             res.redirect(302, "/admin200/badApps")
             return;
-        }
+        }else{
         rimraf.sync(`uploads/${theApp[0].appName}`);
         const result = await Promise.resolve(Apps.deleteAppsByAdmin(req.params.id)).catch(err =>{console.log(err)});
         if(result[0].affectedRows)
         {
-            res.redirect(302, "/admin200/badApps")
+        res.redirect(302, "/admin200/badApps")
         }else{res.redirect(302, "/admin200/badApps")}
 
         if(fs.existsSync(`updates/${theApp[0].appName}`))
         {
             rimraf.sync(`updates/${theApp[0].appName}`);
         }
+    }
        
 
     })
