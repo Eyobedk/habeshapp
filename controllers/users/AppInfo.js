0e9ef59015dd50087331ebbc98346ef89b5a6913
Eyobedk <EyobedKebede10@gmail.com>
2022-05-31 13:53:14 +0100

    rate app only if the app is downloaded by this user
    rate app only if the app is downloaded by this user

--- controllers/users/AppInfo.js
+++ controllers/users/AppInfo.js
@@ -1,4 +1,7 @@
+"use strict";
+
 const Apps = require('../../models/App')
+const {checkDownloadStatus} = require('../../controllers/users/process_Downloader')
 const {
     checkAppRated,
     Rate1star,
@@ -36,18 +39,16 @@ async function handleRating(appId, amount) {
     }
 }
 
-async function performRate(theStarts)
-{
+async function performRate(theStarts) {
     let keyvalue = {
         '1': theStarts[0].oneStar,
         '2': theStarts[0].twoStar,
-        '3':theStarts[0].threeStar,
-        '4':theStarts[0].fourStar,
-        '5':theStarts[0].fiveStar
+        '3': theStarts[0].threeStar,
+        '4': theStarts[0].fourStar,
+        '5': theStarts[0].fiveStar
     };
-    
-    for(const key in keyvalue)
-    {
+
+    for (const key in keyvalue) {
         total += keyvalue[key];
         sum += keyvalue[key] * parseInt(key)
     }
@@ -63,86 +64,120 @@ exports.AppInfo = async (req, res) => {
     let catagory;
 
     await Apps.ListAppsForUsers(appIds).then((result) => {
-        ApkInfo.push([
-            { appname: result[0].appName }, { dev_id: result[0].dev_id }, 
-            { Rate: result[0].appRate }, { func1: result[0].funcOne },
-            { func2: result[0].funcTwo }, { func3: result[0].funcThree },
-            { func4: result[0].funcFour }, { Icon: '/' + result[0].icon },
-            { screenshot1: '/' + result[0].screenshotOne },
-            { screenshot2: '/' + result[0].screenshotTwo },
-            {  screenshot3: '/' + result[0].screenshotThree},
-            { app_id: result[0].appid},
-            {descriptions: result[0].description},
-            {downloads: result[0].downloads}
+        ApkInfo.push([{
+                appname: result[0].appName
+            }, {
+                dev_id: result[0].dev_id
+            },
+            {
+                Rate: result[0].appRate
+            }, {
+                func1: result[0].funcOne
+            },
+            {
+                func2: result[0].funcTwo
+            }, {
+                func3: result[0].funcThree
+            },
+            {
+                func4: result[0].funcFour
+            }, {
+                Icon: '/' + result[0].icon
+            },
+            {
+                screenshot1: '/' + result[0].screenshotOne
+            },
+            {
+                screenshot2: '/' + result[0].screenshotTwo
+            },
+            {
+                screenshot3: '/' + result[0].screenshotThree
+            },
+            {
+                app_id: result[0].appid
+            },
+            {
+                descriptions: result[0].description
+            },
+            {
+                downloads: result[0].downloads
+            }
         ])
         catagory = result[0].catagory;
     }).catch(err => {
         console.log(err)
-    }).then(async ()=>{
+    }).then(async () => {
         const SimilarApps = await Apps.SimilarApps(catagory);
-        for(let i = 0; i< SimilarApps.length; i++)
-        {
+        for (let i = 0; i < SimilarApps.length; i++) {
             similarAppsList.push([
-                
-                {name: SimilarApps[i].appName.replace(/[_]+/g, ' ')},
-                {icon: SimilarApps[i].icon},
-                {rate: SimilarApps[i].appRate},
-                {id: SimilarApps[i].appid}
+
+                {
+                    name: SimilarApps[i].appName.replace(/[_]+/g, ' ')
+                },
+                {
+                    icon: SimilarApps[i].icon
+                },
+                {
+                    rate: SimilarApps[i].appRate
+                },
+                {
+                    id: SimilarApps[i].appid
+                }
             ])
         }
         console.log("res.locals.userEmail")
         console.log(res.locals.userEmail)
-    res.render('users/appPage', {
-        AppInfos: ApkInfo,
-        similarApks: similarAppsList
-         });
+        res.render('users/appPage', {
+            AppInfos: ApkInfo,
+            similarApks: similarAppsList
+        });
     })
 }
 
 exports.AddRate = async (req, res) => {
-    
+
     const apkId = req.params.appkid;
     const RateAmount = req.params.rateAmount;
-    if(RateAmount > 5)
-    {
+    if (RateAmount > 5) {
         res.redirect('back')
     }
-    
-     if(typeof checkRated != 'undefined'){
-    await checkAppRated(apkId).then(async appResult => {
-        if (appResult.length == 0) {
-            await insertRate(apkId).then(async () => {
-                Promise.resolve(await handleRating(apkId,RateAmount)).then(async () => {
-                    await addUserId(apkId, res.locals.userId)
-                }).then(
-                    await updateMyRate(apkId).then(async theStarts => {
-                        await performRate(theStarts).then(result => {
-                            SetRate(result, apkId)
-                        })
-                    })
-                )
-            })
-        } else {
-            await checkRateStatus(apkId, res.locals.userId).then(async StatResult => { //check user rated the app  ?
-                if (StatResult.length == 0) {
-                    await insertRate(apkId).then(async () => {
-                        Promise.resolve(await handleRating(apkId, RateAmount)).then(async () => {
-                            await addUserId(apkId, res.locals.userId)
-                        }).then(
-                            await updateMyRate(apkId).then(async theStarts => {
-                                await performRate(theStarts).then(result => {
-                                    SetRate(result, apkId)
-                                })
-                            })
-                           
-                        )
-                    })
-                }
-            })
-        }
+
+    const StatResult = await checkDownloadStatus(apkId, RateAmount).catch((err) => {
+        console.log(err)
     })
-}
-    
+    if (StatResult.length == 0) {
+        await checkAppRated(apkId).then(async appResult => {
+            if (appResult.length == 0) {
+                await insertRate(apkId).then(async () => {
+                    Promise.resolve(await handleRating(apkId, RateAmount)).then(async () => {
+                        await addUserId(apkId, res.locals.userId)
+                    }).then(
+                        await updateMyRate(apkId).then(async theStarts => {
+                            await performRate(theStarts).then(result => {
+                                SetRate(result, apkId)
+                            })
+                        })
+                    )
+                })
+            } else {
+                await checkRateStatus(apkId, res.locals.userId).then(async StatResult => { //check user rated the app  ?
+                    if (StatResult.length == 0) {
+                        await insertRate(apkId).then(async () => {
+                            Promise.resolve(await handleRating(apkId, RateAmount)).then(async () => {
+                                await addUserId(apkId, res.locals.userId)
+                            }).then(
+                                await updateMyRate(apkId).then(async theStarts => {
+                                    await performRate(theStarts).then(result => {
+                                        SetRate(result, apkId)
+                                    })
+                                })
+
+                            )
+                        })
+                    }
+                })
+            }
+        })
+    }
     res.redirect('back');
-    // next();
 }
