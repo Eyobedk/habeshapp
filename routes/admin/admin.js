964a004489686a5a30ba360d448048c75701e07b
Eyobedk <EyobedKebede10@gmail.com>
2022-06-13 08:32:05 +0100

    added forgot email validator
    added forgot email validator

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
 const {logMeIn, register} = require('../../controllers/admin/Login')
+const {requireAuthforAdmin} = require('../../middleware/auth')
 const {Logout_Admin}= require('../../controllers/admin/Logout_Admin')
-const {sendEmail} = require('../../controllers/admin/sendEmail')
-const {resetAdminPassword} = require('../../controllers/admin/resetAdminPassword')
-const {LoadUsersAndDevelopers} = require('../../controllers/admin/loadUsersandDevs')
+const {sendEmail, resetAdminPassword} = require('../../controllers/admin/sendEmail')
+const {sendUrl} = require('../../controllers/admin/resetAdminPassword')
+const {LoadUsersAndDevelopers,Homepage,LoadAllUsers,ThismonthPublished,ThisMonthNewUsers,ThisMonthNewDevs} = require('../../controllers/admin/loadUsersandDevs')
 const {ListofBadApps} = require('../../controllers/admin/listMaleciousApps')
 const {ListBadDevelopers} = require('../../controllers/admin/ListBadDevelopers')
 const {BlackListDeveloper} = require('../../controllers/admin/BlackListDeveloper')
@@ -18,28 +19,33 @@ router.post('/admin200/login', logMeIn);
 // router.get('/admin200/reg', register);
 
 // WILL BE USED LETTER
-// router.get('/admin200/forgot', (req, res)=>{res.render('admin/forgot')});
-// router.post('/admin200/forgot', sendEmail);
-// router.get('/admin200/reset/:id/:token', (req, res)=>{res.render('admin/reset')});
-// router.post('/admin200/reset/', resetAdminPassword);
+router.get('/admin200/forgot', (req, res)=>{res.render('admin/forgot')});
+router.post('/admin200/forgot', sendEmail);
+router.get('/admin200/resetpass/:id/:token', sendUrl);//(req, res)=>{res.render('admin/reset')}
+router.post('/admin200/resetpass/', resetAdminPassword);
 
 
-router.get('/admin200/badApps', ListofBadApps)
-router.get('/admin200/removeApp/:id', deleteReportedApp)
-router.get('/admin200/badDevelopers', ListBadDevelopers)
-router.get('/banDev/:devID', BlackListDeveloper)
+router.get('/admin200/badApps',requireAuthforAdmin, ListofBadApps)
+router.get('/admin200/removeApp/:id',requireAuthforAdmin, deleteReportedApp)
+router.get('/admin200/badDevelopers', requireAuthforAdmin, ListBadDevelopers)
+router.get('/banDev/:devID', requireAuthforAdmin, BlackListDeveloper)
 
-router.get('/admin200/alertUser', (req, res)=>{res.render('admin/Alert_User')})
-router.post('/admin200/alertUser', AlertUser)
-router.post('/admin200/deleteAlert', DeleteAlert)
+router.get('/admin200/alertUser',requireAuthforAdmin,  (req, res)=>{res.render('admin/Alert_User')})
+router.post('/admin200/alertUser',requireAuthforAdmin, AlertUser)
+router.post('/admin200/deleteAlert', requireAuthforAdmin, DeleteAlert)
 
-router.get('/admin200/alertDev', (req, res)=>{res.render('admin/Alert_Developer')})
-router.post('/admin200/alertDev', AlertDeveloper)
-router.post('/admin200/deleteDevAlert', DeleteAlertofDev)
+router.get('/admin200/alertDev', requireAuthforAdmin, (req, res)=>{res.render('admin/Alert_Developer')})
+router.post('/admin200/alertDev',requireAuthforAdmin, AlertDeveloper)
+router.post('/admin200/deleteDevAlert',requireAuthforAdmin, DeleteAlertofDev)
 router.get('/logout-Admin', Logout_Admin)
 
 // Home panel
 
-router.get('/admin200', LoadUsersAndDevelopers);
+router.get('/admin200', requireAuthforAdmin, Homepage);//(req, res)=>{res.render('admin/pannel')}
+router.get('/listofDevelopers', requireAuthforAdmin, LoadUsersAndDevelopers);
+router.get('/ListofAllusers', requireAuthforAdmin, LoadAllUsers);
+router.get('/ListofThismonthapps', requireAuthforAdmin, ThismonthPublished);
+router.get('/ListofThismontUsers', requireAuthforAdmin, ThisMonthNewUsers);
+router.get('/ThismonthnewDevs', requireAuthforAdmin, ThisMonthNewDevs);
 
 module.exports = router;
