51bce7a0b0988bd6529466e17db6aa1250aecbc2
Eyobedk <EyobedKebede10@gmail.com>
2022-06-23 04:06:24 +0100

    used bad error handler incase shit happens
    used bad error handler incase shit happens

--- controllers/developer/contViews.js
+++ controllers/developer/contViews.js
@@ -5,19 +5,16 @@ exports.contViews = async (req, res, next)=>{
     const userId = res.locals.userId;
     // const TheAppClass = new Apps(req.params.appid);
     
-    const theUser = await Apps.GetAlreadyVistedId(userId, req.params.appid);
+    const theUser = await Apps.GetAlreadyVistedId(userId, req.params.appid, res);
     if(theUser.length == 0)
     {
         await Apps.updateViewCount(req.params.appid).then(async()=>{
-            await Apps.UpdateAlreadyVistedId(userId, req.params.appid).catch((err)=>{
-                console.log(err)
-                if(err.errno == 1452)
-                {
-                    res.redirect(302,'/home');
-                }
+            await Apps.UpdateAlreadyVistedId(userId, req.params.appid, res).catch((err)=>{
+                console.log(err);
             })
         }).catch((err)=>{
             console.log(err)
+            
         });
 
     }
